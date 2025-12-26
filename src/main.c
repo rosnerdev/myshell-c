0d633d58176a5d7ba65ac6b901460830e5b8d00f
rosnerdev <yrosner123@gmail.com>
2025-12-26 11:38:38 +0200

    Trial #1: Second Stage

--- src/main.c
+++ src/main.c
@@ -4,8 +4,15 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  
+
   printf("$ ");
+
+  // stage 2 assumption: everything produces an error, even valid commands
+  // TODO: change this to use actual structured code that works correctly and not just works based on simple assumptions
+  char buf[100]; /* temp variable where the command line goes for now, TODO: FIXIT */
+  scanf("%99s", buf);
+
+  printf("%s: command not found\n", buf);
 
   return 0;
 }
