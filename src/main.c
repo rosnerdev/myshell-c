8823b8ca00cc09639421595da61921ca8da988fb
rosnerdev <yrosner123@gmail.com>
2025-12-26 12:22:59 +0200

    Trial #1: Fifth Stage

--- src/main.c
+++ src/main.c
@@ -8,16 +8,23 @@ int main(int argc, char *argv[]) {
 
   while (1) {
     printf("$ ");
-    // stage 2/3 assumption: everything produces an error, even valid commands
-    // TODO: change this to use actual structured code that works correctly and not just works based on simple assumptions
+    
     char input[100];
     if (!fgets(input, 100, stdin))
       break;
 
-    if (!strncmp(input, "exit", 4))
-      break;
+    input[strcspn(input, "\n")] = '\0';
       
-    input[strlen(input) - 1] = '\0';
+    if (!strcmp(input, "exit"))
+      break;
+    if (!strncmp(input, "echo", 4) && strlen(input) > 5) {
+      const char *rest = strchr(input, ' ') + 1;
+      if (rest != NULL)
+        puts(rest);
+
+      continue;
+    }
+
     printf("%s: command not found\n", input);
   }
 
